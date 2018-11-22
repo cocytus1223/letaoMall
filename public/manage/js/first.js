43cb4de60deaa4effa9ed772497852171a93be4e
Nocky <cocytus1223@gmail.com>
2018-11-22 15:49:35 +0800

    addFirst

--- public/manage/js/first.js
+++ public/manage/js/first.js
@@ -35,4 +35,58 @@ $(function () {
       }
     })
   }
+
+  // 点击添加按钮，显示添加模态框
+  $('#addBtn').click(function () {
+    $('#addModal').modal("show");
+  })
+
+  // 表单校验
+  $('#form').bootstrapValidator({
+    // 配置小图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok', // 校验成功
+      invalid: 'glyphicon glyphicon-remove', // 校验失败
+      validating: 'glyphicon glyphicon-refresh' // 校验中
+    },
+
+    // 配置字段
+    fields: {
+      categoryName: {
+        // 配置校验规则
+        validators: {
+          // 配置非空
+          notEmpty: {
+            message: "请输入一级分类名称"
+          }
+        }
+      }
+    }
+  })
+
+  // 注册表单校验成功事件，阻止默认的表单提交，通过ajax提交
+  $('#form').on("success.form.bv", function (e) {
+    // 阻止默认的提交
+    e.preventDefault();
+    // 通过 ajax 提交
+    $.ajax({
+      type: "post",
+      url: "/category/addTopCategory",
+      data: $('#form').serialize(),
+      dataType: "json",
+      success: function (info) {
+        console.log(info);
+        if (info.success) {
+          // 添加成功
+          // 关闭模态框
+          $('#addModal').modal("hide");
+          // 重新渲染页面, 重新渲染第一页
+          currentPage = 1;
+          render();
+          // 内容和状态都要重置
+          $('#form').data("bootstrapValidator").resetForm(true);
+        }
+      }
+    })
+  })
 })
